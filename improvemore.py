dbe961ed42b15feebf21af113714cffafadd65fa
nouaim <mebarkinouaim@gmail.com>
2022-01-27 11:14:53 +0100

    using cross validation and Random Forest model

---
+++ improvemore.py
@@ -0,0 +1,50 @@
+import pandas as pd
+data = pd.read_csv('melb_data.csv')
+print(data.head())
+print(data.shape)
+
+
+
+
+
+
+y = data.Price
+
+Features = ['Rooms', 'Distance', 'Bedroom2', \
+        'Bathroom', 'Landsize', 'BuildingArea', 'YearBuilt', \
+'Lattitude','Longtitude']
+X = data[Features]
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+
+
+from sklearn.metrics import mean_absolute_error
+train_X, val_X, train_y, val_y= train_test_split(X,y, random_state=0)
+
+
+
+
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+
+from sklearn.model_selection import cross_val_score
+
+def get_mae(train_X, val_X, train_y, val_y):
+    
+    my_pipeline = Pipeline(steps=[('preprocessor', SimpleImputer()),
+                              ('model', RandomForestRegressor(n_estimators=50,
+                                                              random_state=0))
+                             ])
+
+    #model.fit(train_X, train_y)
+    #prediction = model.predict(val_X)
+
+    scores = -1 * cross_val_score(my_pipeline, train_X, train_y,
+                              cv=5,
+                              scoring='neg_mean_absolute_error')
+    print("MAE scores:\n", scores)
+    
+    #Error = mean_absolute_error(prediction, val_y)
+    return scores
+get_mae(train_X, val_X, train_y, val_y)
